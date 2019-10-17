3c35ff8496e6d026b4bd8554b97db4bc22a51290
chandan kumar <chandan.kumar@teradata.com>
2019-10-17 16:34:04 +0530

    first commit

---
+++ src/main/java/SumNumber/Sum.java
@@ -0,0 +1,21 @@
+package SumNumber;
+
+public class Sum {
+	int num1;
+	int num2;
+	
+	public Sum(int a, int b) {
+		this.num1 =a;
+		this.num2 =b;
+	}
+	
+	public int sumofnumbers(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public int sumofnumbers() {
+		return this.num1 + this.num2;
+	}
+
+}
---
+++ src/main/java/SumNumber/SumTest.java
@@ -0,0 +1,12 @@
+package SumNumber;
+
+public class SumTest {
+	
+	public static void main(String a[])
+	{
+		Sum s = new Sum(2,3);
+		System.out.print("sum is ");
+		System.out.println(s.sumofnumbers());
+	}
+
+}
