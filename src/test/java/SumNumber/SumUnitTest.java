1ccf412fb1a5beb749cafc2140fc66344643217e
chandan kumar <chandan.kumar@teradata.com>
2019-10-18 12:26:55 +0530

    unit test commit

---
+++ src/test/java/SumNumber/SumUnitTest.java
@@ -0,0 +1,19 @@
+package SumNumber;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class SumUnitTest {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void constructorTest() {
+		Sum s = new Sum(2,3);
+		assertNotNull(s);
+	}
+}
